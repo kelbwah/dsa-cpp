1d6a9a9db51b5bd6c4d09df0f87a6931b9f0dd5e
kelbwah <kbebops@gmail.com>
2024-09-19 23:24:40 -0700

    Added removeAt function and REPL

--- src/LinkedList/LinkedList.cpp
+++ src/LinkedList/LinkedList.cpp
@@ -1,16 +1,70 @@
 #include "LinkedList.hpp"
+#include <iostream>
+#include <string>
+
+void PrintREPLUsage() {
+    std::cout << "----- Commands List -----" << std::endl;
+    std::cout << "(1) Append to list" << std::endl;
+    std::cout << "(2) Remove first occurrence of value from list" << std::endl;
+    std::cout << "(3) Remove [i]th element from list" << std::endl;
+    std::cout << "(4) Pop from list" << std::endl;
+    std::cout << "(5) Print list" << std::endl;
+    std::cout << "(6) Clear list" << std::endl;
+    std::cout << "(7) Exit" << std::endl;
+    std::cout << "\nYour choice: ";
+}
+
+int GetUserInput(const std::string& message) {
+    int value;
+    std::cout << message;
+    std::cin >> value;
+    std::cout << std::endl;
+    return value;
+}
 
 void LinkedListInit() {
     LinkedList list;
 
-    list.append(1);
-    list.append(2);
-    list.append(3);
-    list.append(4);
-    list.append(5);
-    list.pop();
-    list.pop();
-    list.pop();
-    list.pop();
-    list.pop();
+    bool isRunning = true;
+    while (isRunning) {
+        int input;
+        PrintREPLUsage();
+        std::cin >> input;
+        std::cout << std::endl;
+        std::string message;
+        int userInputVal;
+        switch (input) {
+            case 1:
+                message = "Enter a value to append to list: ";
+                userInputVal = GetUserInput(message);
+                list.append(userInputVal);
+                break;
+            case 2:
+                message = "Enter a value to remove from the list: ";
+                userInputVal = GetUserInput(message);
+                list.remove(userInputVal);
+                break;
+            case 3:
+                message = "Enter an index to remove from the list: ";
+                userInputVal = GetUserInput(message);
+                list.removeAt(userInputVal);
+                break;
+            case 4:
+                list.pop();
+                break;
+            case 5:
+                list.printList();
+                break;
+            case 6:
+                list.clearList();
+                break;
+            case 7:
+                isRunning = false;
+                list.clearList();
+                break;
+            default:
+                std::cout << "Invalid input, review usage." << std::endl;
+                break;
+        }
+    }
 }
--- src/LinkedList/LinkedList.hpp
+++ src/LinkedList/LinkedList.hpp
@@ -1,21 +1,53 @@
 #pragma once
 #include <iostream>
 
+// LinkedList REPL
 void LinkedListInit();
 
+/*
+ * LinkedList Class
+ */
 class LinkedList {
+private:
+    struct Node {
+        int data;
+        Node* next;
+        Node* prev;
+    };
+
+    /*
+     * Creating new node given a data parameter
+     */
+    Node* createNode(const int data) {
+        auto* newNode = new Node();
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+
+        return newNode;
+    }
+
+    // Member vars for head, tail, and size (private)
+    Node* m_head;
+    Node* m_tail;
+    int m_size;
 public:
+    // Constructor
     LinkedList() {
         m_head = nullptr;
         m_tail = nullptr;
         m_size = 0;
-        std::cout << "Initialized empty linked list" << std::endl;
+        std::cout << "(Linked List Initialized)\n" << std::endl;
     };
 
+    // Destructor
     ~LinkedList() {
         clearList();
     };
 
+    /*
+     * Appends a new node to the end of the list given a data parameter
+     */
     void append(const int data) {
         Node* newNode = createNode(data);
         if (m_head == nullptr && m_tail == nullptr) {
@@ -32,24 +64,23 @@ public:
             m_tail->prev = oldTail;
         }
         m_size++;
-        printList();
     }
 
+    /*
+     * Pops the last value of the Linked List if not empty
+     */
     void pop() {
         std::cout << "Popping..." << std::endl;
 
         if (m_size == 0) {
-            std::cout << "Cannot pop from empty list" << std::endl;
+            std::cout << "Cannot pop from empty list\n" << std::endl;
             return;
         }
 
         int poppedVal;
         if (m_size == 1) {
             poppedVal = m_head->data;
-
-            delete m_head;
-            m_head = nullptr;
-            m_tail = nullptr;
+            clearList();
         } else {
             poppedVal = m_tail->data;
             Node* oldTail = m_tail;
@@ -62,13 +93,112 @@ public:
 
         m_size--;
         std::cout << "Popped Value: " << poppedVal << "\n" << std::endl;
-        printList();
     }
 
+    /*
+     * Helper function to remove the head if the head if m_size > 1
+     */
+    void deleteHead() {
+        Node* oldHead = m_head;
+        Node* newHead = m_head->next;
+        newHead->prev = nullptr;
+        m_head = newHead;
+        delete oldHead;
+        m_size--;
+    }
+
+    /*
+     * Helper function to remove a Node (as long as the node is not the head)
+     */
+    void removeNode(Node* nodeToBeRemoved) {
+        Node* newPrev = nodeToBeRemoved->prev;
+        Node* newNext = nodeToBeRemoved->next;
+        if (newNext != nullptr) {
+            newNext->prev = newPrev;
+        }
+        newPrev->next = newNext;
+
+        delete nodeToBeRemoved;
+        m_size--;
+    }
+
+    /*
+     * Removes a node at certain index
+     */
+    void removeAt(const int index) {
+        if (m_size == 0) {
+            std::cout << "Cannot remove from empty list\n" << std::endl;
+            return;
+        }
+
+        if (index > m_size || index < 0) {
+            std::cout << "Index out of bounds\n" << std::endl;
+            return;
+        }
+
+        if (m_size > 1 && index == 0) {
+            deleteHead();
+            return;
+        }
+
+        int counter = 0;
+        Node* currNode = m_head;
+        while (counter <= m_size) {
+            if (counter == index) {
+                removeNode(currNode);
+                return;
+            }
+            currNode = currNode->next;
+            counter++;
+        }
+    }
+
+    /*
+     * Removes the first instance of a target value in the Linked List
+     */
     void remove(const int value) {
-        std::cout << "Removing " << value << "..." << std::endl;
+        std::cout << "----- Removing ----\n" << value << "\n" << std::endl;
+        if (m_size == 0) {
+            std::cout << "Cannot remove from empty list\n" << std::endl;
+            return;
+        }
+
+        if (m_size == 1) {
+            if (m_head->data == value) {
+                clearList();
+            } else {
+                std::cout << "Value not found\n" << std::endl;
+            }
+
+            return;
+        }
+
+        // If the head is the value to be removed
+        if (m_head->data == value) {
+            Node* oldHead = m_head;
+            Node* newHead = m_head->next;
+            newHead->prev = nullptr;
+            m_head = newHead;
+            delete oldHead;
+            m_size--;
+
+            return;
+        }
+
+        Node* currNode = m_head;
+        while (currNode != nullptr) {
+            if (currNode->data == value) {
+                removeNode(currNode);
+                return;
+            }
+            currNode = currNode->next;
+        }
+        std::cout << "Value not found\n" << std::endl;
     }
 
+    /*
+     * Helper function to visualize the Linked List
+     */
     void printList() {
         if (m_size >= 1) {
             std::cout << "Linked List: (size = " << m_size << ")" << std::endl;
@@ -79,11 +209,27 @@ public:
             }
             std::cout << currNode->data << "\n" << std::endl;
         } else {
-            std::cout << "Linked List is empty!" << std::endl;
+            std::cout << "Linked List is empty!\n" << std::endl;
         }
     }
 
+    /*
+     * Helper function to clear the Linked List and deallocate memory
+     */
     void clearList() {
+        if (m_size == 0) {
+            return;
+        }
+
+        if (m_size == 1) {
+            delete m_head;
+            m_head = nullptr;
+            m_tail = nullptr;
+            m_size = 0;
+
+            return;
+        }
+
         Node* currNode = m_head;
         while (currNode != nullptr) {
             Node* next = currNode->next;
@@ -94,24 +240,4 @@ public:
         m_head = nullptr;
         m_tail = nullptr;
     }
-private:
-    struct Node {
-        int data;
-        Node* next;
-        Node* prev;
-    };
-
-    Node* createNode(const int data) {
-        auto* newNode = new Node();
-        newNode->data = data;
-        newNode->next = nullptr;
-        newNode->prev = nullptr;
-
-        std::cout << "\n----- Creating new node ------\n" << newNode->data << "\n\n" << std::endl;
-        return newNode;
-    }
-
-    Node* m_head;
-    Node* m_tail;
-    int m_size;
 };
